5681e80052121fa9e78336c96134cc0b019abb0d
austin <d.austin.vance@gmail.com>
2018-10-20 15:02:40 -0600

    added board and column dropdowns, label multiselect dropdown and functionality

--- js/controllers/formController.js
+++ js/controllers/formController.js
@@ -1,4 +1,40 @@
 angular.module('form')
-.controller('formController', function($scope, $state, trelloService) {
+.controller('formController', function($scope) {
+
+    $scope.card = {
+        board: "Board",
+        column: "Column",
+        labels: []
+    };
+
+    function init() {
+
+    }
+
+    init();
+
+    $scope.setBoard = function(board) {
+        $scope.card.board = board;
+        if (board === "QA / Implementation") {
+            $scope.card.column = "Validate";
+        }
+    };
+
+    $scope.setColumn = function(column) {
+        $scope.card.column = column;
+    };
+
+    $('.selectpicker').change(function () {
+        $scope.card.labels = $(this).val();
+    });
+
+    $scope.isDevelopment = function() {
+        return $scope.card.board === "Plugin Development";
+    };
+
+    $scope.isQA = function() {
+        return $scope.card.board === "QA / Implementation";
+    };
+
 
 });
